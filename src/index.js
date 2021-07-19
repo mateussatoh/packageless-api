326debd0925eb21b8433bd2883be27a37b4dae5b
Mateus Satoh <mateussatoh@gmail.com>
2021-07-18 21:45:06 -0300

    fix: controller and routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const http = require('http')
-const users = require('./mocks/users')
+
+const UserController = require('./controllers/UserController')
 
 const server = http.createServer((request, response) => {
   console.log(`Request method: ${request.method} | Endpoint: ${request.url}`)
   if(request.url === '/users' && request.method === 'GET'){
-    response.writeHead(200, {'Content-type': 'application/json'})
-    response.end(JSON.stringify(users))
+    UserController.listUsers(request,response)
   } else {
     response.writeHead(404, {'Content-type': 'text/html'})
     response.end(`Cannot ${request.method} ${request.url}`)
